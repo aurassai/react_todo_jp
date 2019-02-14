910fca25349d700ebe63cfa3240c1db7b30b13fc
aurassai <b-raychev@yandex.com>
2019-02-14 17:27:17 +0200

    Updates

---
+++ precache-manifest.74bfba164e0a9fbfe265a2bbb6c48a60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "decbcf944dac63d88344",
+    "url": "/react_todo_jp/static/js/runtime~main.decbcf94.js"
+  },
+  {
+    "revision": "0e6fcb5373447c41d5d9",
+    "url": "/react_todo_jp/static/js/main.0e6fcb53.chunk.js"
+  },
+  {
+    "revision": "217b14637750940c1e1e",
+    "url": "/react_todo_jp/static/js/1.217b1463.chunk.js"
+  },
+  {
+    "revision": "0e6fcb5373447c41d5d9",
+    "url": "/react_todo_jp/static/css/main.33329d6d.chunk.css"
+  },
+  {
+    "revision": "a18b47b18c835e195e2be6b76993445e",
+    "url": "/react_todo_jp/index.html"
+  }
+];
