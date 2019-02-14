e8c481b5ea83013e9215b25dc7667ef3116c43ce
aurassai <b-raychev@yandex.com>
2019-02-14 17:31:26 +0200

    changed about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,8 +5,8 @@ function About() {
         <React.Fragment>
             <h1>About</h1>
             <p>
-                This is the TodoList app v1.0.0. It is part of a React crash
-                course
+                This is the TodoList app v1.0.0. It is made as a React training
+                project, using JSON placeholder as a fake REST API.
             </p>
         </React.Fragment>
     );
